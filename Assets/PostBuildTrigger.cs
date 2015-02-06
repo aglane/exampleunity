cf6701ea60722abac1f4ca872d6d7768e7e4362b
Austin Lane <austin.g.lane@gmail.com>
2015-02-06 11:16:13 -0600

    fix
    fix

--- Assets/PostBuildTrigger.cs
+++ Assets/PostBuildTrigger.cs
@@ -5,7 +5,7 @@
 	public static class PostBuildTrigger {
 		[PostProcessBuild]
 		public static void OnPostprocessBuild(BuildTarget target, string path) {
-		  string projectParent = (Directory.GetParent(Application.dataPath).ToString();
+		  string projectParent = (Directory.GetParent(Application.dataPath)).ToString();
 		  string buildName = Path.GetFileNameWithoutExtension(path);
 			Debug.Log( "ProjectParent: " + projectParent + " build: " + buildName );
     	}
